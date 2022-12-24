1516bb8cca481a4b23c1ce3754d6faee12335368
pagnihotri <prash.agnihotri@gmail.com>
2022-12-24 13:28:47 +0530

    project deployment configured

--- data_dump.py
+++ data_dump.py
@@ -1,14 +1,18 @@
 import pymongo
 import pandas as pd
 import json
-
+from sensor.config import mongo_client
 
 
 # Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-DATAFILE_PATH="https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv"
-DATABASE_NAME='aps'
-COLLECTION_NAME='sensor'
+#client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+#DATAFILE_PATH="https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv"
+#DATABASE_NAME='aps'
+#COLLECTION_NAME='sensor'
+
+DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
 
 if __name__=="__main__":
     df=pd.read_csv(DATAFILE_PATH)
