7cd10cf39541f70fc69ba26418759aaaf3b8b696
pagnihotri <prash.agnihotri@gmail.com>
2023-01-04 14:36:29 +0530

    updated train and batch prediction files

--- data_dump.py
+++ data_dump.py
@@ -5,7 +5,7 @@ from sensor.config import mongo_client
 
 
 # Provide the mongodb localhost url to connect python to mongodb.
-#client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+client = pymongo.MongoClient("mongodb+srv://pagnihotri:root@cluster0.asd2aoy.mongodb.net/?retryWrites=true&w=majority")
 #DATAFILE_PATH="https://raw.githubusercontent.com/avnyadav/sensor-fault-detection/main/aps_failure_training_set1.csv"
 #DATABASE_NAME='aps'
 #COLLECTION_NAME='sensor'
@@ -15,8 +15,9 @@ DATABASE_NAME="aps"
 COLLECTION_NAME="sensor"
 
 if __name__=="__main__":
-    df=pd.read_csv(DATAFILE_PATH)
+    df=pd.read_csv(DATA_FILE_PATH)
     print(f'The number of wors and columns {df.shape}')
     df.reset_index(drop=True,inplace=True)
     json_record=list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
